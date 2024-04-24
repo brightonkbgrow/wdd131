46edf1be02a33583c10cfe11b1d73be6a6391e4f
brightonkbgrow <brightongrow@byui.edu>
2024-04-24 15:44:18 -0600

    asdf

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,3 @@
 const currentYear = document.querySelector("#currentYear");
 const today = new Date();
-currentYear.innerHTML = `getFullYear(): <span class="highlight">${today}</span>`;
+currentYear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;

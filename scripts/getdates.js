5785d3da55562831c44e2beb699396748d1bd77c
brightonkbgrow <brightongrow@byui.edu>
2024-04-24 15:50:06 -0600

    asdf

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,3 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Output the current year in the footer's first paragraph
-document.getElementById('currentYear').textContent = currentYear;
-
-// Get the last modified date of the document
-const lastModified = new Date(document.lastModified);
-
-// Format the last modified date
-const formattedLastModified = lastModified.toLocaleDateString();
-
-// Output the formatted last modified date in the footer's second paragraph
-document.getElementById('lastModified').textContent = formattedLastModified;
+const currentYear = document.querySelector("#currentYear");
+const today = new Date();
+currentYear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;

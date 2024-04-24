51d700a7280daeb63ab0e4b4a0792c56e76615cf
brightonkbgrow <brightongrow@byui.edu>
2024-04-24 15:27:19 -0600

    sdf

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,14 @@
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+
+const pattern = /last_modif\s*=\s*([^;]*)/;
+
+const lastVisit = parseFloat(document.cookie.replace(pattern, "$1"));
+const lastModif = Date.parse(document.lastModified);
+
+if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
+  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
+    location.pathname
+  }`;
+
+  if (isFinite(lastVisit)) {
+    alert("This page has been changed!");

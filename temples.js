0d974520ef18b0fd70d43c6a7ad728f358f4db7f
brightonkbgrow <brightongrow@byui.edu>
2024-05-03 11:48:26 -0600

    sdf

---
+++ temples.js
@@ -0,0 +1,16 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelectorAll('nav a');
+
+    hamburger.addEventListener('click', function () {
+        navLinks.forEach(link => {
+            link.style.display = link.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+});

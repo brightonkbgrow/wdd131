1989be28d1b77f91e20b1be9e3141ab10cede7df
brightonkbgrow <brightongrow@byui.edu>
2024-05-30 14:38:30 -0600

    asfd

---
+++ project/scripts/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const photoGallery = document.getElementById('photo-gallery');
+    const photos = [
+        'fedexsmall.jpg',
+        'photo2.jpg',
+        'photo3.jpg'
+    ];
+    
+    photos.forEach(photo => {
+        const img = document.createElement('img');
+    }

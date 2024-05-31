cf3331d5e759f085b0b3f75b3bc9964145400852
brightonkbgrow <brightongrow@byui.edu>
2024-05-30 18:33:10 -0600

    df

--- project/scripts/about.js
+++ project/scripts/about.js
@@ -7,8 +7,14 @@ document.getElementById('lastModified').textContent = lastModified;
 function populateAboutPage() {
     const aboutText = `
         <h2>About Fedex</h2>
-        <p>Fedex is a Domestic Shorthair cat with a unique personality. He enjoys exploring and playing with various objects around the house.</p>
-        <p>Despite his mischievous behavior, Fedex is incredibly affectionate and loves cuddling with his owner.</p>
+        <h2>Name</h2>
+        <p>Fedex got his name after being adopted. His previous name was Paul, which was not fitting. He was renamed to Fedex as he paid attention the a large Fedex truck on his first ride home.</p>
+        <h2>Home</h2>
+        <p>Fedex lives in his home on the 3rd floor of an apartment complex. He loves to run up and down the halls at top speed and watch from the window as children play in the park across the street.</p>
+        <h2>Adventures</h2>
+        <p>Fedex goes on many adventures with his favorite people. He waits by the door and will request to leave. He is the harnessed, collared, and taken out on the town where he finds the most uninteresting things to smell.</p>
+        <h2>Favorite Foods</h2>
+        <p></p>
     `;
     document.getElementById('aboutContent').innerHTML = aboutText;
 
